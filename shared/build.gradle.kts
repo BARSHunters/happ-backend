7608793ecbd7ae304490321d77ed384f9ee66ea1
Zerumi <afanasyev09032005@gmail.com>
2025-03-14 04:54:19 +0300

    BACKEND-9 hotfix: добавлена ссылка на библиотеку сериализации в общий модуль

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "io.github.barshunters"
@@ -13,6 +14,7 @@ dependencies {
     implementation("redis.clients:jedis:5.2.0")
     implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     testImplementation(kotlin("test"))
 }
 

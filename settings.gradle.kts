5b00627af39eab828f8e62ed32907c14e5aafd0d
Kirill Krakov <kirill170251@gmail.com>
2025-03-19 21:29:55 +0300

    BACKEND-3: version 0.1 - trying to make correct PR

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,8 @@ rootProject.name = "happ-backend"
 
 include("shared")
 
-include(":auth")
-include(":userData")
+include(":activity")
+include(":weight_history")
 
-project(":auth").projectDir = File("./services/auth")
-project(":userData").projectDir = File("./services/userData")
+project(":activity").projectDir = File("./services/activity")
+project(":weight_history").projectDir = File("./services/weight_history")

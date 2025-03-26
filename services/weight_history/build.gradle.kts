3092311fa04465fbbf0f31c1e2c73ba8bd6db449
Kirill Krakov <kirill170251@gmail.com>
2025-03-26 15:08:55 +0300

    BACKEND-4: try to merge 2

---
+++ services/weight_history/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.10"
+    application
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+}
+
+group = "happ-backend"
+version = "1.1"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("WeightHistoryServiceKt")
+}
+
+dependencies {
+    implementation(project(":shared"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+// Конфигурация ktlint
+ktlint {
+    version.set("1.0.1") // Укажите версию ktlint
+    debug.set(true) // Включите отладочный вывод
+    verbose.set(true) // Включите подробный вывод
+    outputToConsole.set(true) // Вывод результатов в консоль
+    ignoreFailures.set(false) // Остановить сборку при обнаружении ошибок
+    enableExperimentalRules.set(true) // Включите экспериментальные правила
+    filter {
+        exclude("**/generated/**") // Исключите сгенерированные файлы
+    }
+}

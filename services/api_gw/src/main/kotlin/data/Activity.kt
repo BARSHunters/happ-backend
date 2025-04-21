7b3a6b0180b519da7bce1008bd6ead972c94112c
Zerumi <afanasyev09032005@gmail.com>
2025-04-21 15:54:25 +0300

    BACKEND-15 Исправил эндпоинт отправки тренировок

--- services/api_gw/src/main/kotlin/data/Activity.kt
+++ services/api_gw/src/main/kotlin/data/Activity.kt
@@ -1,6 +1,8 @@
 package com.example.data
 
 import kotlinx.serialization.Serializable
+import serializers.LocalDateTimeSerializer
+import java.time.LocalDateTime
 
 @Serializable
 data class APIGatewayToActivityRequest(
@@ -13,8 +15,10 @@ data class APIGatewayToActivityRequest(
 @Serializable
 data class ActivityDTO(
     val duration: String, // format hh:mm:ss
+    val name: String,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val datetime: LocalDateTime,
     val heartRates: List<HeartRate>,
-
 )
 
 @Serializable
